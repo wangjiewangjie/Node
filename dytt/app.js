6affe0dab39759932dc3f352cecca0796448b4a2
wangjieie <wangjie0107.aliyun.com>
2019-09-19 14:22:15 +0800

    循环爬取title

--- dytt/app.js
+++ dytt/app.js
@@ -42,32 +42,3 @@ main = () => {
 }
 
 main(); //运行主函数
-
-getBtLink = (urls, n) => { //urls里面包含着所有详情页的地址
-  console.log("正在获取第" + n + "个url的内容");
-  superagent.get('http://www.ygdy8.net' + urls[n].title, function (sres) {
-    var chunks = [];
-    sres.on('data', function (chunk) {
-      chunks.push(chunk);
-    });
-    sres.on('end', function () {
-      var html = iconv.decode(Buffer.concat(chunks), 'gb2312'); //进行转码
-      var $ = cheerio.load(html, {
-        decodeEntities: false
-      });
-      $('#Zoom td').children('a').each(function (idx, element) {
-        var $element = $(element);
-        btLink.push({
-          bt: $element.attr('href')
-        })
-      })
-      if (n < urls.length - 1) {
-        getBtLink(urls, ++count);
-        //递归
-      } else {
-        console.log("btlink获取完毕！");
-        console.log(btLink);
-      }
-    });
-  });
-}
