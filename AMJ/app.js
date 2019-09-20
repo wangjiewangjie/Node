a675eda6466a957f10e2d6522b8f341d731a24a1
wangjieie <wangjie0107.aliyun.com>
2019-09-20 15:05:27 +0800

    AMJ

--- AMJ/app.js
+++ AMJ/app.js
@@ -3,25 +3,41 @@ const superagent = require('superagent');
 const charset = require('superagent-charset');
 charset(superagent);
 
-superagent.get('https://www.meiju.net/new/video/search/%E9%BE%99%E6%88%98%E5%A3%AB.html').end((error, html) => {
+const express = require('express')
+const app = express()
 
-  if (error) {
-    console.log(error)
-    return;
-  }
 
-  var $ = cheerio.load(html.text);
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
 
-  superagent.get('https://www.meiju.net/' + $('.thumbnail').attr('href')).end((error, html) => {
+app.get('/', (req, res) => {
+  superagent.get('https://www.meiju.net/new/video/search/%E9%BE%99%E6%88%98%E5%A3%AB.html').end((error, html) => {
+
     if (error) {
       console.log(error)
       return;
     }
-  
+
     var $ = cheerio.load(html.text);
-    $('#detail-content ul li a').each((index,element)=>{
-      let $element = $(element);
-      console.log($element.attr('href'))
+
+    superagent.get('https://www.meiju.net/' + $('.thumbnail').attr('href')).end((error, html) => {
+      if (error) {
+        console.log(error)
+        return;
+      }
+
+      var $ = cheerio.load(html.text);
+      var urls=[];
+      $('#detail-content ul li a').each((index, element) => {
+        let $element = $(element);
+        urls.push({
+          url:$element.attr('href')
+        })
+        // console.log($element.attr('href'))
+      })
+      console.log(req.ip)
+      res.send(urls)
     })
   })
 })
