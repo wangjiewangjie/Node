c5ecc0d49f079f6d3e2ef5b59375577ab6f15a44
wangjieie <wangjie0107.aliyun.com>
2019-09-20 16:11:39 +0800

    amj2.0

--- AMJ2.0/routes/index.js
+++ AMJ2.0/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+const cheerio = require('cheerio');
+const superagent = require('superagent');
+const charset = require('superagent-charset');
+charset(superagent);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'spider' });
+router.get('/', function (req, res, next) {
+
+  var keyword = req.query.keyword
+  superagent.get('https://www.meiju.net/new/video/search/' + keyword + '.html').end((error, html) => {
+
+    if (error) {
+      console.log(error)
+      return;
+    }
+    var $ = cheerio.load(html.text);
+    console.log($('.thumbnail').attr('href'))
+    superagent.get('https://www.meiju.net/' + $('.thumbnail').attr('href')).end((error, html) => {
+      if (error) {
+        console.log(error)
+        return;
+      }
+
+      var $ = cheerio.load(html.text);
+      var urls = [];
+      $('#detail-content ul li a').each((index, element) => {
+        let $element = $(element);
+        urls.push({
+          url: $element.attr('href')
+        })
+        console.log($element.attr('href'))
+      })
+      // res.send(urls)
+    })
+  })
+  res.render('index', {
+    title: 'spider'
+  });
 });
 
 module.exports = router;
